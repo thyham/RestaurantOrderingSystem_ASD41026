aea224d97b1f64d5e6cb92099405c487b2ca89bd
TriumphantObituary <148290564+TriumphantObituary@users.noreply.github.com>
2024-09-07 16:54:50 +1000

    Add user
    -Add user
    -Modify pom.xml

---
+++ src/main/java/com/uts/restaurant/model/User.java
@@ -0,0 +1,74 @@
+package com.uts.restaurant.model;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    int id;
+    String fname;
+    String surname;
+    String email;
+    String phoneNo;
+    Boolean isActive;
+    
+    public User(int id, String fname, String surname, String email, String phoneNo, Boolean isActive) {
+        this.id = id;
+        this.fname = fname;
+        this.surname = surname;
+        this.email = email;
+        this.phoneNo = phoneNo;
+        this.isActive = isActive;
+    }
+    
+    public User(String fname, String surname, String email, String phoneNo, Boolean isActive) {
+        this.fname = fname;
+        this.surname = surname;
+        this.email = email;
+        this.phoneNo = phoneNo;
+        this.isActive = isActive;
+    }
+
+    public User(String fname, String surname, String email, String phoneNo) {
+        this.fname = fname;
+        this.surname = surname;
+        this.email = email;
+        this.phoneNo = phoneNo;
+        isActive = true;
+    }
+
+    public int getID() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+    
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public Boolean isActive() {
+        return isActive;
+    }
+
+}
